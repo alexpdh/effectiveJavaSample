337c94a01c8f4519b376c96fd77b0a9d13673cbd
alexpdh <alexpdh@163.com>
2016-12-28 00:44:28 +0800

    新增第4章第19条：接口只用于定义类型。

---
+++ src/com/sample/chapter04/item19/PhysicalConstants.java
@@ -0,0 +1,32 @@
+/**
+ * @Project Name:effectiveJavaSample
+ * @File Name:PhysicalConstants.java
+ * @Package Name:com.sample.chapter04.item19
+ * @Date:2016年12月28日上午12:43:48
+ *
+*/
+
+package com.sample.chapter04.item19;
+/**
+ * @ClassName:PhysicalConstants
+ * @Function:  
+ * @version
+ *
+ * @author pengdh
+ * @date: 2016年12月28日 上午12:43:48
+ */
+//Constant utility class
+public class PhysicalConstants {
+	private PhysicalConstants() {
+	} // Prevents instantiation
+
+	// Avogadro's number (1/mol)
+	public static final double AVOGADROS_NUMBER = 6.02214199e23;
+
+	// Boltzmann constant (J/K)
+	public static final double BOLTZMANN_CONSTANT = 1.3806503e-23;
+
+	// Mass of the electron (kg)
+	public static final double ELECTRON_MASS = 9.10938188e-31;
+}
+
