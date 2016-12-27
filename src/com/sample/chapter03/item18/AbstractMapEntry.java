56c46578b7f01e5baf2deba18684f4f2a7ecbc3e
alexpdh <alexpdh@163.com>
2016-12-28 00:28:42 +0800

    新增第4章第18条：接口优于抽象类

---
+++ src/com/sample/chapter03/item18/AbstractMapEntry.java
@@ -0,0 +1,57 @@
+/**
+ * @Project Name:effectiveJavaSample
+ * @File Name:AbstractMapEntry.java
+ * @Package Name:com.sample.chapter03.item18
+ * @Date:2016年12月28日上午12:10:21
+ *
+*/
+
+package com.sample.chapter03.item18;
+
+import java.util.Map;
+
+/**
+ * @ClassName:AbstractMapEntry
+ * @Function:  骨架的实现
+ * @version
+ *
+ * @author pengdh
+ * @date: 2016年12月28日 上午12:10:21
+ */
+//Skeletal Implementation
+public abstract class AbstractMapEntry<K, V> implements Map.Entry<K, V> {
+	//Primitive operations
+	public abstract K getKey();
+	public abstract V getValue();
+	
+	//Entries in modifiable maps must override this method
+	//在修改的映射条目必须重写这个方法
+	public V setValue(V value) {
+		throw new UnsupportedOperationException();
+	}
+	
+	//Implements the general contract of Map.Entry.equals
+	//实现了map.entry.equals一般合同
+	@Override
+	public boolean equals(Object o) {
+		if(o == this)
+			return true;
+		if(!(o instanceof Map.Entry))
+			return false;
+		Map.Entry<?, ?> arg = (Map.Entry<?, ?>) o;
+		return equals(getKey(), arg.getKey()) && equals(getValue(), arg.getValue());
+	}
+	private static boolean equals(Object o1, Object o2) {
+		return o1 == null ? o2 == null : o1.equals(o2);
+	}
+	//Implements the general contract of Map.Entry.hashCode
+	//实现了map.entry.hashCode一般合同
+	@Override
+	public int hashCode() {
+		return hashCode(getKey()) ^ hashCode(getValue());
+	}
+	private static int hashCode(Object obj) {
+		return obj == null ? 0 : obj.hashCode();
+	}
+}
+
---
+++ src/com/sample/chapter03/item18/IntArrays.java
@@ -0,0 +1,55 @@
+/**
+ * @Project Name:effectiveJavaSample
+ * @File Name:IntArrays.java
+ * @Package Name:com.sample.chapter03.item18
+ * @Date:2016年12月27日下午11:57:35
+ *
+*/
+
+package com.sample.chapter03.item18;
+
+import java.util.AbstractList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @ClassName:IntArrays
+ * @Function:骨架实现的具体实现  
+ * @version
+ *
+ * @author pengdh
+ * @date: 2016年12月27日 下午11:57:35
+ */
+//concrete implementation built atop skeletal implementation
+public class IntArrays {
+	static List<Integer> intArrayAsList(final int[] a) {
+		if(a == null)
+			throw new NullPointerException();
+		return new AbstractList<Integer>() {
+			public Integer get(int i) {
+				return a[i]; //Autoboxing (Item 5)
+			}
+			
+			@Override
+			public Integer set(int i, Integer val){
+				int oldVal = a[i];
+				a[i] = val;
+				return oldVal;
+			}
+			
+			public int size() {
+				return a.length;
+			}
+		};
+	}
+	public static void main(String[] args) {
+		int[] a = new int[10];
+		for (int i = 0; i < a.length; i++)
+			a[i] = i;
+		List<Integer> list = intArrayAsList(a);
+
+		Collections.shuffle(list);
+		System.out.println(list);
+	}
+}
+
